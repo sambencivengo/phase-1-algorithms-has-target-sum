9e40a91b79fe005106e608227ef460ef59282e7b
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-14 10:54:52 -0400

    commit tree 8ae949ff4b8d3cececb6ff1a04b04431df2ba4f6

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
+const { constants } = require('mocha/lib/suite');
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
   Add your pseudocode here
+  // long method, 
+  brute force it
 */
 
 /*
@@ -17,18 +27,18 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log('Expecting: false');
+  console.log('=>', hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
